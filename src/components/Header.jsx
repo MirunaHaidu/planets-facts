cb7d0379578e987564c0c07466fa5e6f66125e9a
Miruna <miruna_haidu@yahoo.com>
2024-02-09 17:26:23 +0200

    add styling for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../style/Header.css'
 
 const Header = ({ planets, handleClick }) => {
     return (
         <header className="header">
-            <h1 className="logo">The planets</h1>
+            <h1 className="logo">THE PLANETS</h1>
             <ul className="planets-list">
                 {planets.map((planet) => (
                     <li key={planet.name} className="planet-name-list" onClick={() => handleClick(planet)}>
